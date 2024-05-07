f8f04b4d19ccd632f5971016b9f0b1fdc9ff394a
siyaphe <siyapdje@yahoo.fr>
2024-05-07 18:19:16 -0400

    useState commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,81 @@
 import './App.css';
 import  {learners}  from './models/learners'; // my data in js
 import Learner from './components/Learner';  //function in jsx
-
+import { useState } from 'react'; // our useState
 
 function App() {
+
+  const [learnerData, setLearnerData] = useState(learners) 
+  // learners: [{},{},{}]    // --map
+
+   const[user, setUser] = useState({
+      isLoggedIn: true,
+      status: "hidden",
+      content: null,
+      active: true,
+    });
+  
+    // ?: what if we wanted to update/ change the state
+    const  updateState = () => {
+    // 1. Duplicate using [...user] spread
+      console.log('Currently Updating State') 
+      setUser((user)=>({
+        ...user, 
+        // 2. update changes 
+        status: "visible",
+      }))
+     
+};
+
+const updateMultiple = () =>{
+   // 1. Duplicate using [...user] spread
+   console.log('Currently Updating State') 
+   setUser((user)=>({
+     ...user, 
+     // 2. update {MUTIPLE} changes 
+     status: "visible",
+     content: "new Updated Content"
+   }))
+}
+
+const addLearner = () => {
+  let instructor = {
+    name: "Henri Siyapdje",
+    bio: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum",
+    scores: [
+      {
+        date: "2018-02-08",
+        score: 177,
+      },
+      {
+        date: "2018-04-22",
+        score: 192,
+      },
+      {
+        date: "2018-09-15",
+        score: 168,
+      },
+    ],
+  };
+  setLearnerData([
+      ...learnerData,
+     instructor
+    
+  ]);
+};
+
   return (
     
     <div className="App">
-      {learners.map((student)=>{
+      {learnerData.map((student)=>{
         return (
           <>
             <Learner data={student}/>
             <div className='btnContainer'>
-              <button>Update State</button>
-              <button>Add Learner</button>
-              <button>Something Else</button>
+              <button onClick={updateState}>Update State</button>
+              <button onClick={updateMultiple}>Multiple</button>
+              <button onClick={addLearner}>Add Learner</button>
+              <button>Replace Obj</button>
 
             </div>
           
